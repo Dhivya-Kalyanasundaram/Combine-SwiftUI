a674cfba722738f6545b134d8b18432ef8bd3005
Dhivya-Kalyanasundaram <dhivya.k1507@gmail.com>
2025-03-06 12:33:06 +0530

    Initial Commit

---
+++ Combine SwiftUI/Combine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_SwiftUIApp.swift
+//  Combine SwiftUI
+//
+//  Created by Kalyanasundaram, Dhivya (Cognizant) on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Combine SwiftUI/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Combine SwiftUI
+//
+//  Created by Kalyanasundaram, Dhivya (Cognizant) on 26/02/25.
+//
+
+import SwiftUI
+import Combine
+
+struct Post: Decodable {
+    let id: Int
+    let title: String
+    let body: String
+}
+class PostViewModel: ObservableObject {
+    // Published property to notify view of data changes
+    @Published var posts: [Post] = []
+    
+    private var cancellable: AnyCancellable?
+    
+    init() {
+        fetchPosts()
+    }
+    
+    func fetchPosts() {
+        // Replace with your API endpoint URL
+        let urlString = "https://jsonplaceholder.typicode.com/posts"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        // Create a URLSession data task publisher
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data) // Extract data from response
+            .decode(type: [Post].self, decoder: JSONDecoder()) // Decode JSON into array of Posts
+            .replaceError(with: []) // Replace errors with an empty array
+            .receive(on: DispatchQueue.main) // Receive on main queue to update UI
+            .sink(receiveValue: { [weak self] fetchedPosts in
+                self?.posts = fetchedPosts // Update posts with fetched data
+            })
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel = PostViewModel()
+    var body: some View {
+        NavigationView {
+            List(viewModel.posts, id: \.id) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .font(.body)
+                        .foregroundColor(.gray)
+                }
+            }
+            .navigationTitle("Posts")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
